5dde81c10c50d8646f6d129c6af6b19f111e7893
Patrycja Miekina <patrycja.miekina@madetech.com>
2022-09-15 12:20:27 +0100

    Lecture 72 complete

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -23,6 +24,7 @@ const Expenses = (props) => {
           selectedYear={filteredYear}
           onFilterSelect={filterSelectHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
